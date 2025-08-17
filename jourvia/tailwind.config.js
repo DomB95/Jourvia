89df7f01e8e2b3b4c347f42e8a2646d3e26f70a8
DomBie <drb0708@hotmail.com>
2025-08-16 22:10:35 -0400

    updated files ready for coding

---
+++ jourvia/tailwind.config.js
@@ -0,0 +1,22 @@
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3BAFDA',
+        accent: {
+          coral: '#F45B69',
+          yellow: '#FFD166',
+        },
+        neutral: {
+          dark: '#2F3A4C',
+          light: '#F9FAFB',
+        },
+        text: {
+          base: '#4B5563',
+          secondary: '#9CA3AF',
+        }
+      }
+    }
+  }
+};
